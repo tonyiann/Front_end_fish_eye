5245c864ef335f839291135b79eed5b8f532dc22
tonyiann <118492530+tonyiann@users.noreply.github.com>
2023-04-05 19:57:48 +0200

    Add files via upload
    faire apparaitre info sur chaque page de chacun des photographes.

--- scripts/factories/media_factories.js
+++ scripts/factories/media_factories.js
@@ -1,5 +1,5 @@
 function mediaFactory(data) {
-    const { id, photographerId, title, image, likes, date, price, video  } = data;
+    const { id, photographerId, title, image, likes, date, price, video,name,  } = data;
     console.log("photographerid :",photographerId)
 
     
@@ -7,9 +7,30 @@ function mediaFactory(data) {
     const picture = `assets/catalogue/${photographerId}/${image}`;
     console.log("picture:", picture)
     const movie = `assets/catalogue/${photographerId}/${video}`;
+
+    const profil = `assets/photographers/${photographerId}/${portrait}`;
+    
+    const nom = `data/photographers.json/${name}`;
+    console.log("name :" ,name)
+    
+    const local = `data/photographers.json/${localisation}`;
+
+    const slogan = `data/photographers.json/${tagline}`;
+
+
+    
+    
+
+
+
+
     function getMediaCardDOM() {
         const article = document.createElement('article');
         const photo = document.createElement('div');
+
+        const nom = document.createElement( 'h2' );
+        console.log("h2:",h2)
+        nom.textContent = name;
         
 
         console.log("image :",image)
@@ -17,8 +38,8 @@ function mediaFactory(data) {
          {
             const vide = document.createElement('video');
             const source = document.createElement('source');
-            source.setAttribute('src',movie)
-            vide.appendChild(source)
+            source.setAttribute('src',movie);
+            vide.appendChild(source);
             photo.appendChild(vide);
          }
          else {
@@ -30,14 +51,19 @@ function mediaFactory(data) {
          }
          console.log("photo:",photo)
          article.appendChild(photo);
+         article.appendChild(img);
+         
+
          return article;
+         
+
+
 
     }
 
-
     if(image == undefined){
-        return { id, photographerId, title, movie, likes,date, price, video, getMediaCardDOM }
+        return { id, photographerId, title, movie, likes,date, price, video,name, getMediaCardDOM }
     } else{
-        return { id, photographerId, title, picture, likes,date, price, video, getMediaCardDOM }
+        return { id, photographerId, title, picture, likes,date, price, video,name, getMediaCardDOM }
     }
 }
